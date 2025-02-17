259468c0fb8cd002a7d7213b307475b7c57cd090
Adam Fratino <fratino.a@gmail.com>
2025-02-16 23:48:58 -0500

    slot stuff

--- src/components/slots.tsx
+++ src/components/slots.tsx
@@ -1,6 +1,7 @@
 import { cloneElement } from "react";
 
 import { Box, type BoxProps } from "./box";
+import { Conditional } from "./conditional";
 
 type Slot = React.ReactElement<any>;
 
@@ -18,6 +19,8 @@ export const Slots = ({
   display = "inline-flex",
   ...props
 }: SlotsProps) => {
+  const hasSlot = Boolean(before || after);
+
   /** @todo pass props? */
   const beforeElement =
     before && cloneElement(before, undefined, before.props.children);
@@ -27,11 +30,14 @@ export const Slots = ({
     after && cloneElement(after, undefined, after.props.children);
 
   return (
-    <Box data-uiid-layout="slots" display={display} {...props}>
+    <Conditional
+      condition={hasSlot}
+      wrapper={<Box data-uiid-layout="slots" display={display} {...props} />}
+    >
       {beforeElement}
       {children}
       {afterElement}
-    </Box>
+    </Conditional>
   );
 };
 
