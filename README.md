530d6a061823ce250d36e496a47a904f3729cbb4
Adam Fratino <fratino.a@gmail.com>
2025-04-13 19:35:24 -0400

    boolean updates

---
+++ README.md
@@ -0,0 +1,8 @@
+## Install
+
+1. `pnpm i @uiid/layout`
+
+## Configure
+
+1. add core styles to your app
+   a. `import "@uiid/layout/styles.css`
--- src/components/box.tsx
+++ src/components/box.tsx
@@ -12,13 +12,40 @@ export type BoxProps = React.HTMLAttributes<HTMLElement> &
     ref?: React.Ref<any>;
   };
 
-export const Box = ({ render, children, ...props }: BoxProps) => {
-  const styles = styleProps(props, STYLE_PROPS);
-  const variants = booleanProps(props, TOGGLE_PROPS);
+export const Box = ({ render, children, ...allProps }: BoxProps) => {
+  const styles = styleProps(allProps, STYLE_PROPS);
+  const variants = booleanProps(allProps, TOGGLE_PROPS);
+
+  // Destructure all the toggle and style props to remove them
+  // Create an array of all the props we want to filter out
+  const togglePropKeys = Object.keys(TOGGLE_PROPS) as Array<
+    keyof typeof TOGGLE_PROPS
+  >;
+  const stylePropKeys = Object.keys(STYLE_PROPS) as Array<
+    keyof typeof STYLE_PROPS
+  >;
+
+  // Create a new props object without the toggle/style props
+  // This avoids TypeScript errors by using a record
+  const filteredProps: Record<string, unknown> = { ...allProps };
+
+  // Remove toggle props
+  togglePropKeys.forEach((key) => {
+    if (key in filteredProps) {
+      delete filteredProps[key as string];
+    }
+  });
+
+  // Remove style props
+  stylePropKeys.forEach((key) => {
+    if (key in filteredProps) {
+      delete filteredProps[key as string];
+    }
+  });
 
   const propsWithUiid = {
     "data-uiid-layout": "box",
-    ...props,
+    ...filteredProps,
     style: { ...styles, ...variants },
   };
 
@@ -26,7 +53,7 @@ export const Box = ({ render, children, ...props }: BoxProps) => {
     return cloneElement(render, {
       ...propsWithUiid,
       children: children ?? render.props.children,
-      className: cx(props.className, render.props.className),
+      className: cx(allProps.className, render.props.className),
     });
   }
 
